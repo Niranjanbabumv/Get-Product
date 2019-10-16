625b00ace2307d2eedecdab44fe00af3e0c58d22
prem09jun <prem09jun@gmail.com>
2019-10-17 00:16:03 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,218 @@
+/* Angular Module Definition */
+var myApp = angular.module('myModule', ['oitozero.ngSweetAlert']);
+
+/* File Upload Directive */
+myApp.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function () {
+                scope.$apply(function () {
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+
+/* File Upload Service */
+myApp.service('fileUpload', ['$http', function ($http) {
+	return {
+		uploadFileAndFieldsToUrl: function(file, uploadUrl) {
+			var fd = new FormData();
+			fd.append('file', file);
+			return $http.post(uploadUrl, fd, {headers: {'Content-Type': undefined}, transformRequest: angular.identity});
+		}
+	};
+}]);
+
+/* Main Page Form Controller */
+myApp.controller('mainPage', ['SweetAlert', '$scope', 'fileUpload', '$http', '$filter', '$window', function (SweetAlert, $scope, fileUpload, $http, $filter, $window) {		
+	
+	$scope.startShopping = function(){
+		$window.location.href= '/shopping.html';
+    }
+	
+	$scope.startChatBot = function(){
+		$window.location.href= '/chatbot.html';
+    }
+	
+}]);
+
+/* Classify Image Form Controller */
+myApp.controller('classifyImage', ['SweetAlert', '$scope', 'fileUpload', '$http', '$filter', '$window', function (SweetAlert, $scope, fileUpload, $http, $filter, $window) {	
+    $scope.$watch('myFile', function (newFileObj) {
+        if (newFileObj)
+            $scope.filename = newFileObj.name;
+    });	
+	
+	$scope.confirm = function(title, text, type){
+		SweetAlert.swal({title: title, text: text, type: type},function(){ 
+						$window.location.href= '/index.html';
+				   });
+    }
+	
+	$scope.error = function(title, text, type){
+		SweetAlert.swal({title: title, text: text, type: type});
+    }
+
+    $scope.classifyImgVisual = function () {
+        $scope.isDisabled = true;
+		$scope.list = [];
+ 		var file = $scope.myFile;
+        var uploadUrl = "/classifyImg";
+		fileUpload.uploadFileAndFieldsToUrl(file, uploadUrl).then(function (uploadResponse) {
+			if(uploadResponse.data.result){
+				var classes = uploadResponse.data.result.images[0].classifiers[0].classes;
+				var score = 0;
+				var category = '';
+				for(var i=0; i<classes.length; i++){
+					if(classes[i].score > score){
+						score = classes[i].score;
+						category = classes[i].class;
+					}
+				}
+				if(category != ''){
+					var data = {
+						data: category
+					}				
+					$http({
+						method: 'POST',
+						url: '/getProductsAsPerCategory',
+						data: data
+					}).then(function successCallback(response) {
+						if (response.data.success == true) {
+							if(response.data.result && response.data.result.length > 0){
+								for (var i = 0; i < response.data.result.length; i++) {
+									var map = new Object();
+									map['id'] = response.data.result[i]._id;
+									map['imgUrl'] = "data:image/gif;base64,"+response.data.result[i].fileBase64Data;
+									$scope.list.push(map);
+								}
+							}else{
+								$scope.error('No related products found as per the uploaded image category !!', '', 'error');
+							}							
+						} else {
+							$scope.error(response.data.message, '', 'error');
+						}
+					});
+				}else{
+					$scope.error('Product category is not defined in the model !!', '', 'error');
+				}
+			}else{
+				$scope.error('Issues classifying the image !!', '', 'error');
+			}
+		});
+	}		
+
+    $scope.openProductDetails = function (productId) {
+        $scope.isDisabled = true;
+
+        var data = {
+            data: productId
+        }
+
+        $http({
+            method: 'POST',
+            url: '/getProductIdData',
+            data: data
+        }).then(function successCallback(response) {
+            if (response.data.success == true && response.data.result) {
+				$scope.currentProductData = response.data.result[0];
+				$scope.id = $scope.currentProductData._id;
+				$scope.productImgUrl = "data:image/gif;base64,"+$scope.currentProductData.fileBase64Data;
+				$scope.cost = $scope.currentProductData.productCost;
+				$scope.details = $scope.currentProductData.productDescription;
+				var list = [];
+				for(var j=1; j<=$scope.currentProductData.productQuantity; j++){
+					list.push(j);
+				}
+				$scope.accountId = $scope.currentProductData.accountId;
+				$scope.quantity = list;
+				$scope.size = $scope.currentProductData.productSize;
+            } else {
+				$scope.error(response.data.message, '', 'error');
+            }
+        });		
+		
+	}
+    $scope.calculatePrice = function () {
+		$scope.totalCost = $scope.productCount * $scope.cost;
+	}
+	
+    $scope.buyProduct = function (productId) {
+
+        var orderData = {
+			_id: 'O-'+Date.now(),
+            productId: $scope.id,
+			accountId: $scope.accountId,
+			quantity: $scope.productCount,
+			totalCost: $scope.totalCost,
+			dateOfOrderPlaced: Date.now(),
+			productDescription: $scope.details
+        }
+		
+		var data = {
+			data : orderData,
+			updateData : $scope.currentProductData
+		}
+
+        $http({
+            method: 'POST',
+            url: '/addOrderDataToDB',
+            data: data
+        }).then(function successCallback(response) {
+            if (response.data.success == true) {
+				$scope.confirm('Order placed successfully with order id as '+response.data.response.id, '', 'success');			
+            } else {
+                $scope.error(response.data.message, '', 'error');
+            }
+        });		
+		
+	}
+}]);
+
+/* Chat Bot Controller */
+myApp.controller('chatbotController', ['SweetAlert', '$rootScope', '$scope', '$compile', 'fileUpload', '$http', '$filter', '$window', function (SweetAlert,  $rootScope, $scope, $compile, fileUpload, $http, $filter, $window) {
+
+	//Overlay On
+	$scope.on = function () {
+		document.getElementById("overlay").style.display = "block";
+	}
+	//Overlay Off
+	$scope.off = function () {
+		document.getElementById("overlay").style.display = "none";
+	}
+	//Start Chat
+	$scope.startChat = function () {
+		$scope.chatUserName = $scope.userName;
+		$scope.chatStartTime = new Date();
+		var time = new Date().getHours() + ':' + new Date().getMinutes();
+		$scope.off();
+		$scope.createBotChatBox($scope.chatUserName, 'Hi ' + $scope.chatUserName + ' . Can u please provide me with the order Id for your product.', time);
+	}
+	//Send Chat
+	$scope.sendChat = function () {
+		$scope.chatUserName = $scope.userName;
+		$scope.chatStartTime = new Date();
+		var time = new Date().getHours() + ':' + new Date().getMinutes();
+		$scope.createUserChatBox($scope.chatUserName, $scope.userMsg, time);
+	}
+	// Create chat box window for user
+	$scope.createUserChatBox = function (userName, textData, time) {
+		var elementToAdd = angular.element(document.querySelector('#chatbot'));
+		var html = '<div class="row"><div class="col-lg-12"><div class="media"><a class="pull-left" href="#"><img class="media-object img-circle img-chat" src="./img/user.png" alt=""></a><div class="media-body"><h4 class="media-heading">' + userName + '<span class="small pull-right">' + time + '</span></h4><p>' + textData + '</p></div></div></div></div>';
+		elementToAdd.append(html);
+		$compile(elementToAdd)($scope);
+	};
+	// Create chat box window for bot
+	$scope.createBotChatBox = function (userName, textData, time) {
+		var elementToAdd = angular.element(document.querySelector('#chatbot'));
+		var html = '<div class="row"><div class="col-lg-12"><div class="media"><a class="pull-left" href="#"><img class="media-object img-circle img-chat" src="./img/bot.png" alt=""></a><div class="media-body"><h4 class="media-heading">Sales Bot<span class="small pull-right">' + time + '</span></h4><p>' + textData + '</p></div></div></div></div><hr>';
+		elementToAdd.append(html);
+		$compile(elementToAdd)($scope);
+	};
+}]);
---
+++ server.js
@@ -0,0 +1,235 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+var port = process.env.PORT || process.env.VCAP_APP_PORT || '8080';
+var app = express();
+var multer = require('multer');
+var Cloudant = require('@cloudant/cloudant');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+var upload = multer({
+    dest: __dirname + '/upload'
+});
+var type = upload.single('file');
+
+app.use('/', express.static(__dirname + '/'));
+
+var visualRecognition = new VisualRecognitionV3({
+	version: '2018-03-19',
+	iam_apikey: 'BT3UU2CBKHaCPljVZAzT4ZogBlhgsW3Z2G17vdWVPQgq'
+});
+
+var cloudantUserName = '57034c71-949b-496a-a201-c531d9e0f5b3-bluemix';
+var cloudantPassword = '4ba5f7b783631c8538f18f50773288414d5ff1edbe0a5dd0dfe9b29543a2dfd6';
+var cloudant_url = 'https://' + cloudantUserName + ':' + cloudantPassword + '@' + cloudantUserName + '.cloudant.com';
+var cloudant = Cloudant(cloudant_url);
+var dbForProductInfo = cloudant.db.use('product_info');
+var dbForOrderInfo = cloudant.db.use('order_info');
+
+//Starting page when server starts
+app.get('/', function (req, res) {
+    console.log('Open index.html page');
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+//Create index on product db for product category field if not existing
+var productCategory = {
+    name: 'productCategory',
+    type: 'json',
+    index: {
+        fields: ['productCategory']
+    }
+};
+dbForProductInfo.index(productCategory, function (er, response) {
+    if (er) console.log('Error creating index of product category : ' + er);
+    else console.log('Index creation result of product category : ' + response.result);
+});
+//Create index on product db for product id field if not existing
+var _id = {
+    name: '_id',
+    type: 'json',
+    index: {
+        fields: ['_id']
+    }
+};
+dbForProductInfo.index(_id, function (er, response) {
+    if (er) console.log('Error creating index of product id : ' + er);
+    else console.log('Index creation result of product id : ' + response.result);
+});
+//Create index on order db for orderId field if not existing
+var orderId = {
+    name: '_id',
+    type: 'json',
+    index: {
+        fields: ['_id']
+    }
+};
+dbForOrderInfo.index(orderId, function (er, response) {
+    if (er) console.log('Error creating index on orderId : ' + er);
+    else console.log('Index creation result on orderId : ' + response.result);
+});
+/* Classify image to find the uploaded image category */
+app.post('/classifyImg', type, function (req, res) {
+    console.log('Inside Express api to classify image to find the category');
+	var images_file= fs.createReadStream(__dirname + '/upload/' + req.file.filename);
+	var classifier_ids = ["acme-vr-nb_117556229"];
+	var threshold = 0;
+	var params = {
+		images_file: images_file,
+		classifier_ids: classifier_ids,
+		threshold: threshold
+	};
+	visualRecognition.classify(params, function(err, response) {
+		if (err) { 
+			res.json({
+				success: false,
+				message: 'Visual recognition connectivity issue !'
+			});
+		} else {
+			fs.unlink(__dirname + '/upload/' + req.file.filename, function (err) {
+				if (!err)
+					console.log('File deleted !');
+				else 
+					console.log('Issue deleting File');
+			});
+            res.json({
+                success: true,
+                message: 'Product category found successfully ! ',
+                result: response
+            });
+		}
+	});
+});
+//Get product data based on category code from DB
+app.post('/getProductsAsPerCategory', type, function (req, res) {
+    console.log('Inside Express api to get all products as per category code');
+    getProductDetails(req.body.data).then(function (data) {
+        if (data.success) {
+            res.json({
+                success: true,
+                message: 'Product data found successfully ! ',
+                result: data.response.docs
+            });
+        } else res.json({
+            success: false,
+            message: 'Cloudant db connectivity issue !'
+        });
+    });
+});
+//Get product data based on product id from DB
+app.post('/getProductIdData', type, function (req, res) {
+    console.log('Inside Express api to get specific product data');
+    getProductDetail(req.body.data).then(function (data) {
+        if (data.success) {
+            res.json({
+                success: true,
+                message: 'Product data found successfully ! ',
+                result: data.response.docs
+            });
+        } else res.json({
+            success: false,
+            message: 'Cloudant db connectivity issue !'
+        });
+    });
+});
+//Add order details to DB
+app.post('/addOrderDataToDB', type, function (req, res) {
+    console.log('Inside Express api to insert details about order');
+    var orderData = req.body.data;
+	var updateData = req.body.updateData;
+	updateData.productQuantity = updateData.productQuantity - orderData.quantity;
+ 	insertOrderData(orderData).then(function (data) {
+	if (data.success){
+		updateCloudantData(updateData).then(function (updateRes) {
+			if (updateRes.success)
+				res.json({ success: true, message: 'Order data inserted successfully !', response: data.response });
+			else
+				res.json({ success: true, message: 'Issue updating data in cloudant for product db !' });
+		})
+	}else
+		res.json({ success: false, message: 'Issue inserting order data !' });	
+	});
+});
+//Fetch all products based on category code from cloudant DB
+var getProductDetails = async (category) => {
+    try {
+        var response = await dbForProductInfo.find({
+            selector: {
+                productCategory: category
+            }
+        });
+        console.log('Product data found successfully ! ');
+        return ({
+            success: true,
+            message: 'Product data found successfully ! ',
+            response: response
+        });
+    } catch (err) {
+        console.log('Product data not present/DB issue ! ' + err);
+        return ({
+            success: false,
+            message: 'Product data not present/DB issue !'
+        });
+    }
+}
+//Fetch product based on product id from cloudant DB
+var getProductDetail = async (productId) => {
+    try {
+        var response = await dbForProductInfo.find({
+            selector: {
+                _id: productId
+            }
+        });
+        console.log('Product data found successfully ! ');
+        return ({
+            success: true,
+            message: 'Product data found successfully ! ',
+            response: response
+        });
+    } catch (err) {
+        console.log('Product data not present/DB issue ! ' + err);
+        return ({
+            success: false,
+            message: 'Product data not present/DB issue !'
+        });
+    }
+}
+// Insert order details in cloudant DB
+var insertOrderData = async (data) => {
+    try {
+		var data = await dbForOrderInfo.insert(data);
+		console.log('Order Info Inserted !');
+		return ({
+			success: true,
+			message: 'Order Info Inserted Successfully !',
+			response: data
+		});
+    } catch (err) {
+        console.log('Issue fetching/inserting data from DB ! ' + err);
+        return ({
+            success: false,
+            message: 'Issue fetching/inserting data from DB !'
+        });
+    }
+}
+// Update existing product record in cloudant DB
+var updateCloudantData = async (data) => {
+	try {
+		var response = await dbForProductInfo.insert(data);
+        console.log('Applicant data updated successfully ! ');
+        return ({
+            success: true,
+            message: 'Applicant data updated successfully ! '
+        });
+    } catch (err) {
+        console.log('Applicant data updation issue ! ' + err);
+        return ({
+            success: false,
+            message: 'Applicant data updation issue !'
+        });
+    }
+}
+app.listen(port);
